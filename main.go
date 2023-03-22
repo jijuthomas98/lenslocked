ec376038068db51a169d0dd1847a81ca54fcb078
Jiju Thomas <jiju@jify.co>
2023-03-22 12:47:26 +0530

    chor: named template added in home.gohtml

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
 	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
-	r.Get("/contact", controllers.StaticHandler(tpl))
+	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
